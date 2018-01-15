19cd9fd2c1ee04fd05f46bc4c6ccfe195650d108
M.Fevzi Korkutata <fevzimfk@yahoo.com>
2018-01-15 23:34:36 +0300

    Create shutdownManagedServer.sh

---
+++ shutdownManagedServer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Author: M.Fevzi Korkutata | fevzi.korkutata@admineer.com
+/Users/admineer/u01/Oracle/Middleware1221/Oracle_Home/oracle_common/common/bin/wlst.sh /Users/admineer/u01/scripts.imac/shutdownMS/shutdownManagedServer.py $1
